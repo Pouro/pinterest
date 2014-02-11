6acf7ed9ebb680f69bb1475e43dc6ce58c75e00f
Pouro <fsb84@mail.ru>
2014-02-11 13:15:11 +0400

    Install Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinterest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Devise
+  config.action_mailer.default_url_options = { :host => 'http://railsgood.herokuapp.com/' }
 end
